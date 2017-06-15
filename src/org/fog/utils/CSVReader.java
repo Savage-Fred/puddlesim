51b6b9ea74924a4caff51a35bf1e7bca6ac41a89
savage-fred <wpm1414@gmail.com>
2017-06-15 17:01:51 -0500

    tabs to spaces

--- src/org/fog/utils/CSVReader.java
+++ src/org/fog/utils/CSVReader.java
@@ -9,79 +9,79 @@ import java.util.ArrayList;
  * <h1>CSV Reader </h1>
  * A class to read in cartesian coordinates from a CSV file 
  * 
- * @author 	William McCarty
+ * @author  William McCarty
  * @version 1.0.0
- * @since 	June 14, 2017
+ * @since   June 14, 2017
  */
 public class CSVReader {
 
-	private String csvFileName;
-	
-	/////////////////////////////////////////////////////////
-	/////////////////// CONSTRUCTORS
-	/////////////////////////////////////////////////////////
+    private String csvFileName;
+    
+    /////////////////////////////////////////////////////////
+    /////////////////// CONSTRUCTORS
+    /////////////////////////////////////////////////////////
 
-	/** 
-	 * <b> Generic Constructor </b>
-	 * Basic constructor for CSVReader 
-	 * @param filenamein the path to the CSV file 
-	 */
-	public CSVReader(String filenamein) {
-		this.setFileName(filenamein);
-	}
+    /** 
+     * <b> Generic Constructor </b>
+     * Basic constructor for CSVReader 
+     * @param filenamein the path to the CSV file 
+     */
+    public CSVReader(String filenamein) {
+        this.setFileName(filenamein);
+    }
 
-	/////////////////////////////////////////////////////////
-	/////////////////// 	GETTERS AND SETTERS
-	/////////////////////////////////////////////////////////
-	public 	void setFileName(String fn) {this.csvFileName = new String(fn);}
-	public 	String getFileName() {return this.csvFileName;}
+    /////////////////////////////////////////////////////////
+    ///////////////////     GETTERS AND SETTERS
+    /////////////////////////////////////////////////////////
+    public  void setFileName(String fn) {this.csvFileName = new String(fn);}
+    public  String getFileName() {return this.csvFileName;}
 
 
 
-	/////////////////////////////////////////////////////////
-	/////////////////// 	METHODS
-	/////////////////////////////////////////////////////////
-	/**
-	 * <b> Parse File Method </b>
-	 * A function to parse a CSV File containing comma delimited cartesian coordinates 
-	 * 		x1,y1
-	 *		x2,y2...
-	 *
-	 * @param 	csvFileName path to the CSV File 
-	 * @return 	an array of cartesian points 
-	 */
-	public static Point[] parseFile(String csvFileName) {
-		BufferedReader br = null;
-		String line = "";
-		String csvSplitBy = ",";
-		ArrayList<Point> pointArrayList = new ArrayList<Point>(50);
-		try {
-			br = new BufferedReader(new FileReader(csvFileName));
-			while((line = br.readLine()) !=null) {
-				String[] row = line.split(csvSplitBy);
-				Double xcoord = Double.parseDouble(row[0]);
-				Double ycoord = Double.parseDouble(row[1]);
-				Point point = new Point(xcoord, ycoord);
+    /////////////////////////////////////////////////////////
+    ///////////////////     METHODS
+    /////////////////////////////////////////////////////////
+    /**
+     * <b> Parse File Method </b>
+     * A function to parse a CSV File containing comma delimited cartesian coordinates 
+     *      x1,y1
+     *      x2,y2...
+     *
+     * @param   csvFileName path to the CSV File 
+     * @return  an array of cartesian points 
+     */
+    public static Point[] parseFile(String csvFileName) {
+        BufferedReader br = null;
+        String line = "";
+        String csvSplitBy = ",";
+        ArrayList<Point> pointArrayList = new ArrayList<Point>(50);
+        try {
+            br = new BufferedReader(new FileReader(csvFileName));
+            while((line = br.readLine()) !=null) {
+                String[] row = line.split(csvSplitBy);
+                Double xcoord = Double.parseDouble(row[0]);
+                Double ycoord = Double.parseDouble(row[1]);
+                Point point = new Point(xcoord, ycoord);
 
-				pointArrayList.add(point);
-			}
-	 	} catch (FileNotFoundException e) {
-	        e.printStackTrace();
-	    } catch (IOException e) {
-	        e.printStackTrace();
-	    } catch (NumberFormatException e) {
-	        e.printStackTrace();
-	    } finally {
-	        if (br != null) {
-	            try {
-	                br.close();
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
-	        }
-		}
+                pointArrayList.add(point);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 
-		Point pointarray[] = pointArrayList.toArray(new Point[pointArrayList.size()]);
-		return pointarray;
-	}
+        Point pointarray[] = pointArrayList.toArray(new Point[pointArrayList.size()]);
+        return pointarray;
+    }
 }
--- src/org/fog/utils/Point.java
+++ src/org/fog/utils/Point.java
@@ -4,40 +4,40 @@ package org.fog.utils;
  * <h1> Point Class </h1>
  * Defines a point in cartesion space 
  *
- * @author 	William McCarty
+ * @author  William McCarty
  * @version 1.0.0
- * @since 	June 14, 2017
+ * @since   June 14, 2017
  */ 
 class Point {
-	//////////////////////////////////////////
-	////////////// 	FIELDS 
-	//////////////////////////////////////////
-	public double x;
-	public double y;
-	
+    //////////////////////////////////////////
+    //////////////  FIELDS 
+    //////////////////////////////////////////
+    public double x;
+    public double y;
+    
 
-	//////////////////////////////////////////
-	////////////// CONSTRUCTORS 
-	//////////////////////////////////////////
+    //////////////////////////////////////////
+    ////////////// CONSTRUCTORS 
+    //////////////////////////////////////////
 
-	/**
-	 * <b> Basic constructor for Point </b>
-	 *
-	 * @param x x coordinate 
-	 * @param y y coordinate
-	 */
-	public Point(double x, double y) {
-		this.setx(x);
-		this.sety(y);
-	}
+    /**
+     * <b> Basic constructor for Point </b>
+     *
+     * @param x x coordinate 
+     * @param y y coordinate
+     */
+    public Point(double x, double y) {
+        this.setx(x);
+        this.sety(y);
+    }
 
 
-	//////////////////////////////////////////
-	////////////// GETTERS AND SETTERS 
-	//////////////////////////////////////////
-	public void 	setx(double input) {this.x = input;}
-	public void 	sety(double input) {this.y = input;}
+    //////////////////////////////////////////
+    ////////////// GETTERS AND SETTERS 
+    //////////////////////////////////////////
+    public void     setx(double input) {this.x = input;}
+    public void     sety(double input) {this.y = input;}
 
-	public double 	getx() {return this.x;}
-	public double 	gety() {return this.y;}
+    public double   getx() {return this.x;}
+    public double   gety() {return this.y;}
 }
