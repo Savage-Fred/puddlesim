0807629b942440562589c0e60595a8aef4bcacbc
Savage-Fred <wpm1414@gmail.com>
2017-07-18 14:37:40 -0500

    Added default capacity value.

--- src/org/fog/utils/Config.java
+++ src/org/fog/utils/Config.java
@@ -9,6 +9,7 @@ public class Config {
 	public static String FOG_DEVICE_ARCH = "x86";
 	public static String FOG_DEVICE_OS = "Linux";
 	public static String FOG_DEVICE_VMM = "Xen";
+	public static double DEFAULT_FLOW_CAPACITY = 10.0;
 	public static double FOG_DEVICE_TIMEZONE = 10.0;
 	public static double FOG_DEVICE_COST = 3.0;
 	public static double FOG_DEVICE_COST_PER_MEMORY = 0.05;
